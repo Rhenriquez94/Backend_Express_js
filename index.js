1b19c7aa16e6ddf8ef5fb641b756efcc1ad84020
Rhenriquez <rhenriquez@Rhenriquez.entelchile.net>
2024-06-29 23:06:02 -0400

    test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 const PORT = 3001; 
 
-app.use(cors()); 
+app.use(cors()) ; 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
